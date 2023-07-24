806fd5ec82207bfb518c825ef16200f699212239
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-07-24 09:31:16 +0200

    First test passed

--- src/cinemaGoing.js
+++ src/cinemaGoing.js
@@ -1,5 +1,21 @@
-function cinemaGoing() {
-  return 43;
+function cinemaGoing(card, ticket, frac) {
+
+  let regularPrice = 0;
+  let schemaPrice = 500;
+  let ticketNumber = 1;
+
+  while(schemaPrice - 15 > regularPrice){
+    regularPrice += ticket;
+    schemaPrice = schemaPrice + 15 * Math.pow(frac, ticketNumber)
+    ticketNumber++;
+  }
+
+  console.log('Regular price: ', regularPrice )
+  console.log('Schema Price: ', schemaPrice)
+
+
+  return ticketNumber;
+
 }
 
 module.exports = cinemaGoing;
